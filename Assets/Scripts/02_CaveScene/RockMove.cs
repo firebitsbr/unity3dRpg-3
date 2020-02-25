8bc223025c1daf7a348d3cb156e345d6ac3c61cd
Administrator <Administrator@DESKTOP-6V7L04O>
2020-02-26 01:00:54 +0900

    ..

--- Assets/Scripts/02_CaveScene/RockMove.cs
+++ Assets/Scripts/02_CaveScene/RockMove.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class RockMove : MonoBehaviour
 { 
+
+    // addforce
     Rigidbody rb;
     public float x = 1f;
     public float z = 0f;
-
     bool isFighter = false;
-    // Start is called before the first frame update
+
+    //공격 사정거리
+    private Transform playerTransform;
+
+    public float traceDist = 1.0f;
+    public float dist = 30f;
+
     void Start()
     {
-        rb = GetComponent<Rigidbody>();  
+        rb = GetComponent<Rigidbody>();
+
+        playerTransform = GameObject.FindWithTag("Player").GetComponent<Transform>();
+
+        StartCoroutine(this.CheckState());
+    }
+
+    IEnumerator CheckState() {
+
+        while (!isFighter)
+        {
+            yield return new WaitForSeconds(0.2f);
+
+            dist = Vector3.Distance(playerTransform.position, transform.position);
+
+            Debug.Log(dist);
+
+            if (dist <= traceDist)
+            {
+                isFighter = true;
+            }
+        }
+
     }
 
     // Update is called once per frame
@@ -21,6 +50,7 @@ public class RockMove : MonoBehaviour
         if(isFighter) rb.AddForce(x, 0f, z);  
     }
 
+/*
     private void OnTriggerEnter(Collider other)
     {
         //플레이어의 공격에 맞았다면.
@@ -31,6 +61,6 @@ public class RockMove : MonoBehaviour
 
         }
     }
-
+*/
 
 }
